c7c67de1c352ecdf015cf2ec484d079c8d248cd6
Obada A.Albadee <obadabadee3@hotmail.com>
2023-09-21 01:07:36 +0300

    Change Directory

--- main.h
+++ main.h
@@ -378,4 +378,11 @@ char *_strchr(char *s, char c);
  */
 int is_equal_found(const char *name);
 
+/**
+ * changeDirectory - Change the current directory 
+ * @newpwd: The path pf the new Directory 
+ * Return: Nothing
+ */
+void changeDirectory(const char *newpwd);
+
 #endif /* MAIN_H */
