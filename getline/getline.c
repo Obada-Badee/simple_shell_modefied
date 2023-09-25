04cf27d7cefb10ffe4be2dc0ac4ee3d13a58ca33
Obada A.Albadee <obadabadee3@hotmail.com>
2023-09-26 02:49:44 +0300

    add line separatiion to the page

--- getline/getline.c
+++ getline/getline.c
@@ -8,14 +8,28 @@
  */
 void _getlines(char **commands, int *lines_count)
 {
-    char line[MAX_LINE_LENGTH];
+    char page[MAX_LINE_LENGTH];
     char *trimmed_line;
     char *end;
+    char *line;
     ssize_t bytes_read;
+    char ch ;
+    int i ; /* to go through buffer */
+    int j = 0 ; /*to got through lines */
 
-    while ((*lines_count < MAX_COMMANDS) && (bytes_read = read(STDIN_FILENO, line, sizeof(line))) > 0)
+    while ((*lines_count < MAX_COMMANDS) && (bytes_read = read(STDIN_FILENO, page, sizeof(page))) > 0)
     {
-        line[bytes_read] = '\0'; /*Null-terminate the line*/
+        page[bytes_read] = '\0'; /*Null-terminate the page*/
+
+        for (i = 0;i<bytes_read;i++)
+        {
+            ch = page[i];
+            if (ch == '\n') {
+                line[j] = '\0';
+            }
+            line[j++] = ch;
+        }
+        j = 0;
 
         /*Remove leading and trailing whitespace*/
         trimmed_line = line;
