c32d54dbcab311f1ef544dd124a14bf7dd5291fd
Obada A.Albadee <obadabadee3@hotmail.com>
2023-09-26 02:28:44 +0300

    try non interactive

--- getline/main.c
+++ getline/main.c
@@ -5,14 +5,18 @@
  *
  * Return: Always 0.
  */
-int main() {
+int main(int argc, char* argv[]) {
     char **commands = malloc(MAX_COMMANDS * sizeof(char *));
     int commandCount = 0;
 
-    write(STDOUT_FILENO, "$ ", 2);
-
+    /*write(STDOUT_FILENO, "$ ", 2);
     _getlines(commands, &commandCount);
-    process_lines(commands, commandCount);
+    process_lines(commands, commandCount);*/
+
+    for (int i = 0; i < argc; i++) 
+    {
+        printf("%s\n", argv[i]);
+    }
 
     return 0;
 }
